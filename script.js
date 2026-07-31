c69cbc6b8053680b0f98bbee8dba6409cca9c2db
DevanshSharmaCL <179486114+DevanshSharmaCL@users.noreply.github.com>
2026-07-31 09:00:51 +0530

    Q-31 -  sum of digit

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const prompt = require("prompt-sync")();
 
 
-// Question - 30 - Print Each Digit of a number
+// Question - 31 - sum of digit 
 
-let num = 456 
-let numStr = num.toString().split('');
-console.log(numStr)
+let n = 1234
+let numStr = n.toString().split('')
+let sum = 0 
 
-for(let i = 0 ; i <numStr.length; i++){
+for(let i=0; i<numStr.length ; i++){
     console.log(numStr[i])
+    // console.log(numStr)
+    sum = sum + Number(numStr[i])
+    console.log(sum)
+    // numStr[i]= nums
 }
