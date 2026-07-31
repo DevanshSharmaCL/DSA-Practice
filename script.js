d1d8265999a56528f02b246505ccfdf3dc0ed5e8
DevanshSharmaCL <179486114+DevanshSharmaCL@users.noreply.github.com>
2026-07-31 08:14:38 +0530

    Q-29 power of a number

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 const prompt = require("prompt-sync")();
 
 
-// Question - 27 - Sum of all the Factor
+// Question - 29 -  power of a number
 
-
-let n = 6 
-let sum = 0
-for (let i = 0 ; i<=6; i++){
-    if(n%i==0){
-        sum += i 
-        console.log(sum)
-    }
-}
+let a = 2 
+let b = 10 
+let result = a**b
+console.log(result)
